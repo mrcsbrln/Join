22c2ae8d59a89312e02a1c53f2988d1fde160bb2
Meik Behr <meik.behr@gmx.de>
2024-06-29 15:46:22 +0200

    update index: cleancoding ES6 and commenting JSDOC

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 "use strict";
 
-
+/**
+ * Initializes the index page by checking for a current user.
+ *
+ * Checks if there is a current user logged in using session storage.
+ * If a current user exists, redirects to the summary page.
+ * Otherwise, redirects to the login page.
+ */
 function initIndex() {
-
     checkForCurrentUser() ? redirectToSummary() : redirectToLogin();
-
 }
-
