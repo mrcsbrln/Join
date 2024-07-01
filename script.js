4888450f8fb38ddcc4debbb4e950addaf2f10556
Kevin Perret <kevin.perret@outlook.de>
2024-07-01 18:56:13 +0200

    console log removed

--- script.js
+++ script.js
@@ -168,7 +168,6 @@ function redirectToSummary() {
 async function loadData(path="") {
 	let response = await fetch(BASE_URL + path + ".json");
 	let responseToJson = await response.json();
-	console.log(responseToJson);
 	return responseToJson;
 }
 
