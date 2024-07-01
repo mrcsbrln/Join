7bbb093186693100d9b8ab443636108fd06a753a
Meik Behr <meik.behr@gmx.de>
2024-07-01 20:52:28 +0200

    update legalNotice.js : put some comments there

--- js/legalNotice.js
+++ js/legalNotice.js
@@ -1,12 +1,19 @@
+/**
+ * Initializes the legal notice section by including HTML content,
+ * hides the navigation bar, header icons, and highlights the legal notice link.
+ */
 function initLegalNotice() {
     includeHTML().then(() => {
         hideNavBar();
         hideHeaderIcons();
         highlightLegalNotice();
- });
+    });
 }
 
 
+/**
+ * Navigates the user back to the previous page in the browsing history.
+ */
 function goBack() {
     window.history.back();
 }
